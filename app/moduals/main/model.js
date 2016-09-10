f9d02ce44d4ea5d30f3904bd30fc29f24f02ff73
gjwlg <guojiawei@gsiworld.com.cn>
2016-09-10 15:00:43 +0800

    加入快捷键提示modal

---
+++ app/moduals/main/model.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Joey on 2016/7/22.
+ */
+define(['../../js/common/BaseModel'],function(BaseModel){
+
+    var homeModel=BaseModel.extend({
+
+        defaults:{
+        },
+
+        goods: function (data, callback) {
+            this.sendPOST({
+                url: window.API_URL.BARGAINGOODS,
+                data: data,
+                success: callback
+            });
+        },
+        sku: function (data, callback) {
+            this.sendPOST({
+                url: window.API_URL.SKU,
+                data: data,
+                success: callback
+            });
+        },
+        logout: function (data, callback) {
+            this.sendPOST({
+                url:window.API_URL.LOGIN,
+                data:data,
+                success:callback
+            })
+        }
+    });
+    return homeModel;
+});
--- app/moduals/main/view.js
+++ app/moduals/main/view.js
@@ -3,8 +3,11 @@
  */
 define([
     '../../../../js/common/BaseView',
+    '../../../../moduals/main/model',
+    '../../../../moduals/main/collection',
+    'text!../../../../moduals/main/posinfotpl.html',
     'text!../../../../moduals/main/tpl.html',
-], function (BaseView, tpl) {
+], function (BaseView, HomeModel, HomeCollection, posinfotpl, tpl) {
 
     var mainView = BaseView.extend({
 
@@ -14,6 +17,14 @@ define([
 
         template: tpl,
 
+        totalamount: 0,
+
+        itemamount: 0,
+
+        discountamount: 0,
+
+        template_posinfo:posinfotpl,
+
         events: {
 
         },
@@ -21,10 +32,33 @@ define([
         pageInit: function () {
             pageId = window.PAGE_ID.MAIN;
             console.log(pageId);
+            var user = storage.get(system_config.LOGIN_USER_KEY);
+            this.model = new HomeModel();
+            this.requestModel = new HomeModel();
+            this.model.set({
+                name:user.user_name,
+                pos: '收款机(2341)',
+                totalamount: this.totalamount,
+                itemamount: this.itemamount,
+                discountamount: this.discountamount
+            });
+            this.initTemplates();
         },
 
         initPlugins: function () {
+            $('input[name = main]').focus();
+            this.renderPosInfo();
+        },
 
+        initTemplates: function () {
+            this.template_posinfo = _.template(this.template_posinfo);
+            //this.template_cart = _.template(this.template_cart);
+            //this.template_shopitem = _.template(this.template_shopitem);
+        },
+
+        renderPosInfo: function () {
+            this.$el.find('.for-posinfo').html(this.template_posinfo(this.model.toJSON()));
+            return this;
         },
 
         bindKeys: function () {
@@ -36,6 +70,9 @@ define([
             });
             this.bindKeyEvents(pageId, window.KEYS.S, function () {
                 router.navigate('salesman',{trigger:true});
+            });
+            this.bindKeyEvents(pageId, window.KEYS.Esc,function () {
+                toastr.info('esc 主页');
             });
         }
 
