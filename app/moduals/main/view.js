c0ce7383aec4202828239ebe75a199c4524e740c
xuying <yingx_xu@163.com>
2016-09-11 13:07:23 +0800

    购物车

--- app/moduals/main/view.js
+++ app/moduals/main/view.js
@@ -8,8 +8,9 @@ define([
     '../../../../moduals/salesman/view',
     'text!../../../../moduals/main/posinfotpl.html',
     'text!../../../../moduals/main/salesmantpl.html',
+    'text!../../../../moduals/main/cartlisttpl.html',
     'text!../../../../moduals/main/tpl.html',
-], function (BaseView, HomeModel, HomeCollection, SalesmanView, posinfotpl,salesmantpl, tpl) {
+], function (BaseView, HomeModel, HomeCollection, SalesmanView, posinfotpl,salesmantpl,cartlisttpl, tpl) {
 
     var mainView = BaseView.extend({
 
@@ -29,6 +30,8 @@ define([
 
         template_salesman:salesmantpl,
 
+        template_cartlisttpl:cartlisttpl,
+
         salesmanView:null,
 
         events: {
@@ -36,10 +39,12 @@ define([
         },
 
         pageInit: function () {
+            var _self = this;
             pageId = window.PAGE_ID.MAIN;
             console.log(pageId);
             var user = storage.get(system_config.LOGIN_USER_KEY);
             this.model = new HomeModel();
+            this.collection = new HomeCollection();
             this.requestModel = new HomeModel();
             this.model.set({
                 name:user.user_name,
@@ -53,6 +58,10 @@ define([
             } else {
                 this.salesmanView = new SalesmanView();
             }
+            if (storage.isSet(system_config.SALE_PAGE_KEY)) {
+                _self.collection.set(storage.get(system_config.SALE_PAGE_KEY, 'shopcart'));
+                _self.model.set(storage.get(system_config.SALE_PAGE_KEY, 'shopinfo'));
+            }
             this.initTemplates();
             this.handleEvents();
         },
@@ -61,15 +70,27 @@ define([
             $('input[name = main]').focus();
             this.renderPosInfo();
             this.renderSalesman();
+            this.renderCartList();
+            this.initLayoutHeight();
         },
 
         initTemplates: function () {
             this.template_posinfo = _.template(this.template_posinfo);
             this.template_salesman = _.template(this.template_salesman);
+            this.template_cartlisttpl = _.template(this.template_cartlisttpl);
             //this.template_cart = _.template(this.template_cart);
             //this.template_shopitem = _.template(this.template_shopitem);
         },
-
+        /**
+         * 初始化layout中各个view的高度
+         */
+        initLayoutHeight: function () {
+            var dh = $(document).height();
+            var nav = $('.navbar').height();
+            var toolbar = $('.label-primary').height();
+            cart = dh - nav - toolbar - 140;
+            $('.for-cartlist').height(cart);
+        },
         renderPosInfo: function () {
             this.$el.find('.for-posinfo').html(this.template_posinfo(this.model.toJSON()));
             return this;
@@ -77,6 +98,10 @@ define([
 
         renderSalesman: function() {
             this.$el.find('.for-salesman').html(this.template_salesman(this.model.toJSON()));
+        },
+
+        renderCartList: function() {
+            this.$el.find('.for-cartlist').html(this.template_cartlisttpl(this.collection.toJSON()));
         },
 
         handleEvents: function () {
@@ -94,9 +119,34 @@ define([
 
         bindKeys: function () {
             var _self = this;
+            this.request = new HomeModel();
             console.log('bindkeys main');
             this.bindKeyEvents(window.PAGE_ID.MAIN, window.KEYS.Enter, function () {
-                console.log('主页');
+                search = $('#input_main').val();
+                if(search == ''){
+                    toastr.warning('您输入的商品编码为空，请重新输入');
+                }else{
+                    var data = {};
+                    data['skucode'] = search;
+                    if(storage.isSet(system_config.VIP_KEY)) {
+                        data['cust_id'] = storage.get(system_config.VIP_KEY,'cust_id');
+                        data['medium_id'] = storage.get(system_config.VIP_KEY,'medium_id');
+                        data['medium_type'] = storage.get(system_config.VIP_KEY,'medium_type');
+                    }else{
+                        data['cust_id'] = '*';
+                        data['medium_id'] = '*';
+                        data['medium_type'] = '*';
+                    }
+                    data['goods_detail'] = JSON.stringify(_self.collection);
+                    _self.request.sku(data , function(resp) {
+                        if(resp.status == '00') {
+                            _self.onAddItem(resp.goods_detail);
+                        }else{
+                            toastr.warning('您输入的商品编码不存在，请重新输入');
+                        }
+                    });
+                    $('#input_main').val('');
+                }
             });
             this.bindKeyEvents(window.PAGE_ID.MAIN, window.KEYS.M, function () {
                 console.log('main m');
@@ -114,7 +164,59 @@ define([
             this.bindKeyEvents(window.PAGE_ID.MAIN, window.KEYS.B,function () {
                 router.navigate('billing',{trigger:true});
             });
-        }
+        },
+
+        onAddItem: function (JSONData) {
+            this.collection.set(JSONData, {merge: false});
+            this.insertSerial();
+            this.calculateModel();
+        },
+
+        /**
+         * 每次添加商品时，向新添加的商品插入serial属性值
+         */
+        insertSerial: function () {
+            for (var i = 0; i < this.collection.length; i++) {
+                this.collection.at(i).set({
+                    serial: i + 1
+                });
+            }
+        },
+
+        /**
+         * 购物车中商品变更后执行的通用方法，主要是在更新后collection中重新计算总计、件数和优惠
+         */
+        calculateModel: function () {
+            this.totalamount = 0;
+            this.itemamount = 0;
+            this.discountamount = 0;
+            var priceList = this.collection.pluck('price');
+            var itemNum = this.collection.pluck('num');
+            var discounts = this.collection.pluck('discount');
+            for (var i = 0; i < this.collection.length; i++) {
+                discounts[i] = parseFloat(discounts[i]);
+                this.totalamount += priceList[i] * itemNum[i];
+                this.itemamount += itemNum[i];
+                this.discountamount += discounts[i];
+            }
+            this.renderCartList();
+            this.updateShopInfo();
+            storage.set(system_config.SALE_PAGE_KEY, 'shopcart', this.collection.toJSON());
+            storage.set(system_config.SALE_PAGE_KEY, 'shopinfo', this.model.toJSON());
+
+        },
+
+        /**
+         * 更新当前销售详情
+         */
+        updateShopInfo: function () {
+            this.model.set({
+                totalamount: this.totalamount,
+                itemamount: this.itemamount,
+                discountamount: this.discountamount
+            });
+            this.renderPosInfo();
+        },
 
     });
 
