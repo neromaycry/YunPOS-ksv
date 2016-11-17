9e760f3b2311706342b8a23417c1e9cbc11060fc
gjwlg <guojiawei@gsiworld.com.cn>
2016-11-17 15:59:41 +0800

    commit

--- app/js/config.js
+++ app/js/config.js
@@ -105,7 +105,7 @@ requirejs([
 
     window.psbar = pscrollbar;
 
-    window.isPacked = true;  //程序是否已打包，打包前必须把此项设置为true
+    window.isPacked = false;  //程序是否已打包，打包前必须把此项设置为true
 
     console.log(document.body.clientWidth);
 
