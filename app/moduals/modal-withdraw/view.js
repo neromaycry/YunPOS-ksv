7d215e7bcb1aa1a0d625ae20e129f2ab69eaee34
gjwlg <guojiawei@gsiworld.com.cn>
2016-11-18 09:30:46 +0800

    增加 各页面logo

--- app/moduals/modal-withdraw/view.js
+++ app/moduals/modal-withdraw/view.js
@@ -29,7 +29,7 @@ define([
             $('.modal').on('shown.bs.modal', function () {
                 $(_self.input).focus();
             });
-            wsClient.send(DIRECTIVES.OpenCashDrawer);
+
         },
 
         onCancelClicked: function () {
@@ -61,12 +61,17 @@ define([
             }
             var pos = '收款机(2341)';
             var userName = storage.get(system_config.LOGIN_USER_KEY, 'user_name');
+            var currentTime = this.getCurrentFormatDate();
+            console.log(currentTime);
             var printText = '\n\n\n\n';
+            printText += '            长宇测试\n\n';
+            printText += '    提款时间' + currentTime;
             printText += '        收款机：' + pos + '\n\n';
             printText += '        收款员：' + userName + '\n\n';
             printText += '        提取金额：' + toDecimal2(money) + ' 元\n\n\n\n';
             printText += '    提款人签字：\n\n\n\n\n\n\n\n';
             wsClient.send(DIRECTIVES.PRINTTEXT + printText);
+            wsClient.send(DIRECTIVES.OpenCashDrawer);
             this.hideModal(window.PAGE_ID.MAIN);
         },
 
@@ -79,6 +84,25 @@ define([
                 _self.hideModal(window.PAGE_ID.MAIN);
                 $('input[name = main]').focus();
             });
+        },
+
+        getCurrentFormatDate: function () {
+            var date = new Date();
+            var seperator1 = "-";
+            var seperator2 = ":";
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            var strDate = date.getDate();
+            if (month >= 1 && month <= 9) {
+                month = "0" + month;
+            }
+            if (strDate >= 0 && strDate <= 9) {
+                strDate = "0" + strDate;
+            }
+            var currentdate = year + seperator1 + month + seperator1 + strDate
+                + " " + date.getHours() + seperator2 + date.getMinutes()
+                + seperator2 + date.getSeconds();
+            return currentdate;
         }
 
     });
