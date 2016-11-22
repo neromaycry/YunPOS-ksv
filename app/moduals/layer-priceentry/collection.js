68873f5113163be64fa8370a1716f5260c115da4
xuying <yingx_xu@163.com>
2016-11-22 15:27:05 +0800

    修改 银行业务、手动录入价格模态框为layer

---
+++ app/moduals/layer-priceentry/collection.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Joey on 2016/7/22.
+ */
+define(['../../js/common/BaseCollection',
+    '../../moduals/layer-priceentry/model',],function(BaseCollection,LayerPriceEntryModel){
+
+    var layerPriceEntryCollection = BaseCollection.extend({
+
+        model:LayerPriceEntryModel,
+
+    });
+
+    return layerPriceEntryCollection;
+});
