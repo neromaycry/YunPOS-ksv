f90f3d61b299e217e3abfae4c640218137388a64
gjwlg <guojiawei@gsiworld.com.cn>
2016-11-30 18:20:14 +0800

    修改 权限获取方式

--- app/moduals/setposkey/view.js
+++ app/moduals/setposkey/view.js
@@ -19,18 +19,18 @@ define([
         input: 'input[name = poskey]',
 
         events: {
-            'click .btn-numpad':'onNumpadClicked',
-            'click .btn-num':'onNumClicked',
-            'click .btn-backspace':'onBackspaceClicked',
-            'click .btn-clear':'onClearClicked',
+            'click .btn-numpad': 'onNumpadClicked',
+            'click .btn-num': 'onNumClicked',
+            'click .btn-backspace': 'onBackspaceClicked',
+            'click .btn-clear': 'onClearClicked',
             //'click .poskey-pre':'onPreClicked',
-            'click .poskey-next':'onNextClicked'
+            'click .poskey-next': 'onNextClicked'
         },
 
         pageInit: function () {
             pageId = window.PAGE_ID.SETPOSKEY;
-            if (!storage.isSet(system_config.SETTING_DATA_KEY,system_config.INIT_DATA_KEY,system_config.GATEWAY_KEY)) {
-                storage.set(system_config.SETTING_DATA_KEY,system_config.INIT_DATA_KEY,system_config.GATEWAY_KEY,'http://111.198.72.128:3000/v1');
+            if (!storage.isSet(system_config.SETTING_DATA_KEY, system_config.INIT_DATA_KEY, system_config.GATEWAY_KEY)) {
+                storage.set(system_config.SETTING_DATA_KEY, system_config.INIT_DATA_KEY, system_config.GATEWAY_KEY, 'http://111.198.72.128:3000/v1');
             }
         },
 
@@ -41,7 +41,7 @@ define([
         bindKeys: function () {
             var _self = this;
             this.bindKeyEvents(window.PAGE_ID.SETPOSKEY, window.KEYS.Enter, function () {
-               _self.onNextClicked();
+                _self.onNextClicked();
             });
             //this.bindKeyEvents(window.PAGE_ID.SETPOSKEY, window.KEYS.Esc, function () {
             //    router.navigate('setdns', {trigger:true});
@@ -51,10 +51,10 @@ define([
         onNumpadClicked: function () {
             var isDisplay = $('.numpad').css('display') == 'none';
             if (isDisplay) {
-                $('.numpad').css('display','block');
+                $('.numpad').css('display', 'block');
                 $('.btn-numpad').text('关闭小键盘');
             } else {
-                $('.numpad').css('display','none');
+                $('.numpad').css('display', 'none');
                 $('.btn-numpad').text('打开小键盘');
             }
         },
@@ -68,7 +68,7 @@ define([
 
         onBackspaceClicked: function () {
             var str = $(this.input).val();
-            str = str.substring(0, str.length-1);
+            str = str.substring(0, str.length - 1);
             $(this.input).val(str);
         },
 
@@ -82,23 +82,27 @@ define([
 
         onNextClicked: function () {
             var _self = this;
-            var value =  $('input[name = poskey]').val();
+            var value = $('input[name = poskey]').val();
             if (value == '') {
                 toastr.warning('请输入有效的授权key');
             } else {
                 var data = {};
                 data['poskey'] = value;
                 this.request = new SetPoskeyModel();
-                this.request.init(data, function(resp) {
-                    if(resp.status == '00') {
-                        console.log(resp);
-                        router.navigate('initinfo', { trigger: true });
-                        //storage.set(system_config.SETTING_DATA_KEY,system_config.INIT_DATA_KEY, 'poskey', value);
-                        storage.set(system_config.SETTING_DATA_KEY, system_config.INIT_DATA_KEY, system_config.POS_KEY, value);
-                        storage.set(system_config.SETTING_DATA_KEY, system_config.INIT_DATA_KEY, system_config.POS_LIMIT, resp.pos_limit);
+                this.request.init(data, function (resp) {
+                    if (!$.isEmptyObject(resp)) {
+                        if (resp.status == '00') {
+                            console.log(resp);
+                            router.navigate('initinfo', {trigger: true});
+                            //storage.set(system_config.SETTING_DATA_KEY,system_config.INIT_DATA_KEY, 'poskey', value);
+                            storage.set(system_config.SETTING_DATA_KEY, system_config.INIT_DATA_KEY, system_config.POS_KEY, value);
+                            //storage.set(system_config.SETTING_DATA_KEY, system_config.INIT_DATA_KEY, system_config.POS_LIMIT, resp.pos_limit);
+                        } else {
+                            //toastr.error(resp.msg);
+                            layer.msg(resp.msg, optLayerError);
+                        }
                     } else {
-                        //toastr.error(resp.msg);
-                        layer.msg(resp.msg, optLayerError);
+                        layer.msg('服务器错误，请联系管理员', optLayerError);
                     }
                 }, function (jqXHR, textStatus, errorThrown) {
                     console.log(textStatus);
