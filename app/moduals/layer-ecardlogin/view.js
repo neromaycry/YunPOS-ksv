b3ffb235dc70dd4c1fcae659d124b13722842d23
gjwlg <guojiawei@gsiworld.com.cn>
2016-12-09 14:28:25 +0800

    修改 支付方式获取方式

--- app/moduals/layer-ecardlogin/view.js
+++ app/moduals/layer-ecardlogin/view.js
@@ -51,7 +51,7 @@ define([
         },
 
         onCancelClicked: function () {
-            if (this.attrs.pageid == 6) {
+            if (this.attrs.pageid == PAGE_ID.BILLING) {
                 $('input[name = billing]').focus();
             } else {
                 $('input[name = billingrt]').focus();
@@ -59,36 +59,36 @@ define([
             this.closeLayer(layerindex);
         },
 
-        onOkClicked: function () {
-            var _self = this;
-            var cardId = $('input[name = ecard]').val();
-            var data = {};
-            data['mobile'] = cardId;
-            data['password'] = '*';
-            data['type'] = this.type;
-            this.request.getVipInfo(data, function (resp) {
-                if (!$.isEmptyObject(resp)) {
-                    if (resp.status == '00') {
-                        var attrs = {
-                            pageid: PAGE_ID.BILLING,
-                            card_id: cardId,
-                            cust_id: resp.cust_id,
-                            unpaidamount: _self.attrs.unpaidamount,
-                            gather_money: _self.attrs.gather_money,
-                            goods_detail: storage.get(system_config.SALE_PAGE_KEY, 'shopcart'),
-                            gather_detail: _self.attrs.gather_detail,
-                            account_type_code: resp.account_type_code
-                        };
-                        _self.closeLayer(layerindex);
-                        _self.openLayer(PAGE_ID.LAYER_ECARD_PAY, PAGE_ID.BILLING, '一卡通支付', LayerECardpayView, attrs, {area: '900px'});
-                    } else {
-                        layer.msg(resp.msg, optLayerError);
-                    }
-                } else {
-                    layer.msg('系统错误，请联系管理员', optLayerWarning);
-                }
-            });
-        },
+        //onOkClicked: function () {
+        //    var _self = this;
+        //    var cardId = $('input[name = ecard]').val();
+        //    var data = {};
+        //    data['mobile'] = cardId;
+        //    data['password'] = '*';
+        //    data['type'] = this.type;
+        //    this.request.getVipInfo(data, function (resp) {
+        //        if (!$.isEmptyObject(resp)) {
+        //            if (resp.status == '00') {
+        //                var attrs = {
+        //                    pageid: PAGE_ID.BILLING,
+        //                    card_id: cardId,
+        //                    cust_id: resp.cust_id,
+        //                    unpaidamount: _self.attrs.unpaidamount,
+        //                    gather_money: _self.attrs.gather_money,
+        //                    goods_detail: storage.get(system_config.SALE_PAGE_KEY, 'shopcart'),
+        //                    gather_detail: _self.attrs.gather_detail,
+        //                    account_type_code: resp.account_type_code
+        //                };
+        //                _self.closeLayer(layerindex);
+        //                _self.openLayer(PAGE_ID.LAYER_ECARD_PAY, PAGE_ID.BILLING, '一卡通支付', LayerECardpayView, attrs, {area: '900px'});
+        //            } else {
+        //                layer.msg(resp.msg, optLayerError);
+        //            }
+        //        } else {
+        //            layer.msg('系统错误，请联系管理员', optLayerWarning);
+        //        }
+        //    });
+        //},
 
         /**
          * 刷卡输入
